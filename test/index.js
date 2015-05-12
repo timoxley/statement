1ee7510f3b1eee2f721ca0f26570158c94377d75
Tim Oxley <secoif@gmail.com>
2015-05-12 10:25:22 +0800

    Update dependencies, use smokestack.

--- test/index.js
+++ test/index.js
@@ -2,3 +2,8 @@ require('./initial')
 require('./actions')
 require('./context')
 require('./normalize')
+
+require('tape')('shutdown', function(t) {
+  setTimeout(function() {window.close()})
+  t.end()
+})
